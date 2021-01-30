6a7677588d107914f2d77ed19ade7cf0de333a15
abhishahasane <abhishahasane@gmail.com>
2021-01-30 21:28:12 +0530

    addition

--- app.py
+++ app.py
@@ -1,11 +1,18 @@
+import os
 from flask import Flask
 from flask_restful import Api
 from resources.krupasindhu import krupasindhuData
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('mysql://KCAL_USR:bapu18112025@localhost/203.192.211.193')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.secret_key = 'abhi'
 api = Api(app)
 
 api.add_resource(krupasindhuData, '/krupasindhu')
 
 if __name__ == "__main__":
+    from db import db
+
+    db.init_app(app)
     app.run(port=5001, debug=True)
